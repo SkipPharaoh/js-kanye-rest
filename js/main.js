c77087ad15f41f0048c43db453a55f66ad5382e8
skippharaoh <caniggiathompson@gmail.com>
2022-01-11 23:12:02 -0500

    Main Goals Completed

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
-console.log("👀")
+
+let url = "https://api.kanye.rest/"
+const options = {
+    methond: "GET",
+    headers: {}
+}
+function newQuote () {
+fetch (url, options).then((resp)=>{
+    console.log(resp)
+    resp.json().then((data) => {
+        console.log("our data", data.quote)
+        displayQuote(data.quote)
+})
+function displayQuote(quote){
+    const kanyeQuote = document.getElementById("container")
+    kanyeQuote.innerText = quote
+}
+})
+}
+newQuote()
+const kanyeButton = document.querySelector(".button")
+kanyeButton.addEventListener("click", newQuote)
